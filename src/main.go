5a331dd85b485bcb3ac8dcb2284b69c47987e7a1
Ty3r0X <ty3r0x@disroot.org>
2024-02-21 10:31:43 +0200

    Preliminary backend shenanigans

--- src/main.go
+++ src/main.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+	"time"
 )
 
 func main() {
+	fmt.Printf("HealthBite backend server\n2024 Patrick Covaci\n%s\n", time.TimeOnly)
+
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "salut bozos: %s\n", r.URL.Path)
+		tmpl, err := template.ParseFiles("static/index.html")
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		err = tmpl.Execute(w, time.TimeOnly)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	})
 
 	http.ListenAndServe(":8080", nil)
