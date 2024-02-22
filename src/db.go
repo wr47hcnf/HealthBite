5f1d43b5e07488853bcfd19accd7a26cef3d6a89
Ty3r0X <ty3r0x@disroot.org>
2024-02-22 11:45:04 +0200

    Further database logic work

--- src/db.go
+++ src/db.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	_ "github.com/lib/pq"
 	"log"
 )
 
@@ -16,13 +17,13 @@ const (
 
 var Db *sql.DB
 
-func init_db() {
-	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s", host, port, user, password, dbname)
+func dbConnect() error {
+	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 	Db, err := sql.Open("postgres", connStr)
 
 	if err != nil {
 		log.Fatal(err)
-		panic(err)
+		return err
 	}
 
 	defer Db.Close()
@@ -31,6 +32,38 @@ func init_db() {
 
 	if err != nil {
 		log.Fatal("Failed to connect to the database: ", err)
-		panic(err)
+		return err
 	}
+
+	return nil
+}
+
+func dbInit() error {
+	var tableExists bool
+	err := Db.QueryRow("SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = 'users')").Scan(&tableExists)
+
+	if err != nil {
+		log.Fatal("Failed to query the database", err)
+		return err
+	}
+
+	if tableExists {
+		log.Println("Database already initialized, continuing")
+		return nil
+	}
+
+	_, err = Db.Exec(`
+	CREATE TABLE users (
+		id SERIAL PRIMARY KEY,
+		username VARCHAR(50) UNIQUE NOT NULL,
+		password VARCHAR(100) NOT NULL)
+   `)
+
+	if err != nil {
+		log.Fatal("Failed to initialize database", err)
+		return err
+	}
+
+	log.Println("Succesfully initialized database")
+	return nil
 }
--- src/init.go
+++ src/init.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"time"
 )
 
 func init() {
+	fmt.Printf("HealthBite backend server\n(C) 2024 Patrick Covaci a.k.a Ty3r0X\n%s\n", time.Now())
 
-	fmt.Printf("HealthBite backend server\n2024 Patrick Covaci\n%s\n", time.Now())
+	err := dbConnect()
+
+	if err != nil {
+		log.Fatal("Failed to connect to database")
+	}
 
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
