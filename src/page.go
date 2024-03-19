5d633e712468f700c96ea0e0e515769f362ceee0
Ty3r0X <ty3r0x@disroot.org>
2024-03-19 15:36:56 +0200

    Further fixes

--- src/page.go
+++ src/page.go
@@ -69,7 +69,7 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	}
 	rows, err := Db.Query("SELECT * FROM productdata")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	defer rows.Close()
 	for rows.Next() {
@@ -88,12 +88,12 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 			&product.ProdAllergens,
 		)
 		if err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
 		pageData.Products = append(pageData.Products, product)
 	}
 	if err := rows.Err(); err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	err = tmpl.Execute(w, pageData)
